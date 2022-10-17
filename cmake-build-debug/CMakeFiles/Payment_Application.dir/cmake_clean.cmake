bcd8c427e1729e81b82eb97de2751f44f293199d
kareemAbd0 <kareemabdo.7781@yahoo.com>
2022-10-17 15:39:55 +0200

    added temp functions for database module

---
+++ cmake-build-debug/CMakeFiles/Payment_Application.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Payment_Application.dir/App/App.c.o"
+  "CMakeFiles/Payment_Application.dir/App/App.c.o.d"
+  "CMakeFiles/Payment_Application.dir/Card/Card.c.o"
+  "CMakeFiles/Payment_Application.dir/Card/Card.c.o.d"
+  "CMakeFiles/Payment_Application.dir/Data_Base/DataBase.c.o"
+  "CMakeFiles/Payment_Application.dir/Data_Base/DataBase.c.o.d"
+  "CMakeFiles/Payment_Application.dir/Server/Server.c.o"
+  "CMakeFiles/Payment_Application.dir/Server/Server.c.o.d"
+  "CMakeFiles/Payment_Application.dir/Terminal/Terminal.c.o"
+  "CMakeFiles/Payment_Application.dir/Terminal/Terminal.c.o.d"
+  "CMakeFiles/Payment_Application.dir/main.c.o"
+  "CMakeFiles/Payment_Application.dir/main.c.o.d"
+  "Payment_Application"
+  "Payment_Application.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Payment_Application.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
